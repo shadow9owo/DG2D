b70619fbd73e08605fa829b4d69ec150bb7a8b3c
shadowdev <69159493+shadow9owo@users.noreply.github.com>
2025-07-02 20:27:45 +0200

    Add files via upload
    universal patch

--- utils/array.h
+++ utils/array.h
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <cstring>
 #include <array>
+#include <limits>
 
 namespace DG2D //chatgpt over engineering this shit could be 10 times simpler this is fucking garbage i fucking hate this fucking anyone whos reading this kill yourself you fucking nigger faggot 
 {
 
-    std::array<int, MAX_SIZE> ConvertCharArrayToIntArray(const char value[MAX_SIZE], size_t& outLength) {
+    std::array<int, INT16_MAX> ConvertCharArrayToIntArray(const char value[INT16_MAX], size_t& outLength) {
         outLength = 0;
-        while (outLength < MAX_SIZE && value[outLength] != '\0') {
+        while (outLength < INT16_MAX && value[outLength] != '\0') {
             ++outLength;
         }
 
-        std::array<int, MAX_SIZE> intArr{};
+        std::array<int, INT16_MAX> intArr{};
         for (size_t i = 0; i < outLength; ++i) {
             intArr[i] = static_cast<int>(value[i]);
         }
