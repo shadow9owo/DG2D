6ce507cce91704cb4419f0d0599e014a2172b9fe
shadowdev <69159493+shadow9owo@users.noreply.github.com>
2025-07-02 20:41:51 +0200

    Add files via upload

---
+++ utils/array.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstring>
+#include <array>
+#include <limits>
+#include <vector>
+
+namespace DG2D
+{
+    extern std::vector<int> ConvertCharArrayToIntArray(const char value[INT16_MAX], size_t& outLength);
+}
