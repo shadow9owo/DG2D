819d7d72768ba8ffc19157a33bf2f370586c34e8
shadowdev <69159493+shadow9owo@users.noreply.github.com>
2025-07-02 21:14:42 +0200

    Add files via upload

---
+++ events/Handle_SDL_events.cpp
@@ -0,0 +1,43 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+#include "Handle_SDL_events.hpp"
+#include <vector>
+
+namespace DG2D
+{
+    std::vector<std::pair<SDL_EVENTS, int>> HandleEvents()
+    {
+        std::vector<std::pair<SDL_EVENTS, int>> events;
+        SDL_Event e;
+
+        while (SDL_PollEvent(&e)) {
+            switch (e.type)
+            {
+                case SDL_QUIT:
+                    events.emplace_back(quit, 0);
+                    break;
+
+                case SDL_KEYDOWN:
+                    events.emplace_back(keydown, e.key.keysym.sym);
+                    break;
+
+                case SDL_KEYUP:
+                    events.emplace_back(keyup, e.key.keysym.sym); 
+                    break;
+
+                case SDL_MOUSEBUTTONDOWN:
+                    events.emplace_back(mousedown, e.button.button);
+                    break;
+
+                case SDL_MOUSEBUTTONUP:
+                    events.emplace_back(mouseup, e.button.button);
+                    break;
+
+                default:
+                    events.emplace_back(none, 0);
+                    break;
+            }
+        }
+        return events;
+    }
+}
---
+++ events/Handle_SDL_events.hpp
@@ -0,0 +1,18 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <vector>
+
+enum SDL_EVENTS
+{
+    none = 0,
+    quit,
+    keydown,
+    keyup,
+    mousedown,
+    mouseup
+};
+
+namespace DG2D
+{
+    extern std::vector<std::pair<SDL_EVENTS, int>> HandleEvents();
+}
