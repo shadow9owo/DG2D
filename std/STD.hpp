9b6a8e611482f6f235078ee7d07b87e018c8d6ec
shadowdev <69159493+shadow9owo@users.noreply.github.com>
2025-09-15 08:19:01 +0200

    Update STD.hpp

--- std/STD.hpp
+++ std/STD.hpp
@@ -7,7 +7,7 @@ namespace DG2D
         extern std::string filename;
     
         extern bool SetValue(std::string Key,std::string Value);
-        extern bool LoadValue(std::string Key);
+        extern std::string LoadValue(std::string Key);
         extern bool HasKey(std::string Key);
     }
 }
